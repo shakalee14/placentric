3520a1de07776188f4a51cd2f73a27aebaf681a9
Shaka Lee <shakalee14@gmail.com>
2017-02-09 02:31:57 -0600

    Created Website Ready for Heroku

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Placentric' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+
+router.get('/contact', function(req, res, next){
+  res.render('contact')
+})
+
+router.get('/services', function(req, res, next){
+  res.render('services')
+})
+
+router.get('/media', function(req, res, next){
+  res.render('media')
+})
+
+module.exports = router;
