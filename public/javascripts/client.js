6fab7ba01c38365ba921751fb7519fff975a4048
Shakalee14 <shakalee14@gmail.com>
2017-02-20 14:45:07 -0800

    WIP

---
+++ public/javascripts/client.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    var header = document.querySelector(".services-header");
+    var text = document.querySelector(".services-text")
+
+  $('.section h2 p').hover(function() {
+	   $(this).children('h2').stop(true, false, true).slideToggle(300);
+   });
+
+});
