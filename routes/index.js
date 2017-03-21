3b741960bdbf9153b6d81b29fb216dd93a4ac522
Shakalee14 <shakalee14@gmail.com>
2017-03-21 16:02:15 -0700

    New style

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('splash-page', { title: 'La Placenta' });
-});
 
-router.get('/home', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index');
 });
 
